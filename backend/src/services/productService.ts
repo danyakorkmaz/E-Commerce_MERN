d0f0fbe3de0ec5545c6ccd59b044cb21207c415a
danyakorkmaz <106125107+danyakorkmaz@users.noreply.github.com>
2025-02-09 22:47:50 +0300

    Cart Page layout

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -10,7 +10,7 @@ export const seedIntitialProducts = async () => {
 try {
     const products =[
         { title:"Dell laptop", image:"https://cdn.webshopapp.com/shops/308128/files/466926742/image.jpg", price:15000, stock:10},
-        { title:"Asus laptop", image:"https://www.amazon.com.tr/ASUS-Notebook-F515EA-EJ3282-i5-1135G7-Graphics/dp/B0BL7QRQHN", price:25000, stock:20},
+        { title:"Asus laptop", image:"https://cdn.webshopapp.com/shops/308128/files/466926742/image.jpg", price:25000, stock:20},
         { title:"HP laptop", image:"https://www.hpstore.com.tr/hp-250-laptop-g9-intel-core-i5-1235u-8gb-ram-512gb-ssd-156-inc-fhd-windows-11-home-gumus-grisi-9m3g4at-15077-19-B.jpg", price:40000, stock:8},
     ];  
 
--- frontend/src/context/Cart/CartProvider.tsx
+++ frontend/src/context/Cart/CartProvider.tsx
@@ -32,15 +32,17 @@ const CartProvider: FC<PropsWithChildren> = ({ children }) => {
 
       const cart = await response.json();
 
-      const cartItemsMapped = cart.items.map(({ product, quantity } : {product: any ; quantity : number}) => ({
+      const cartItemsMapped = cart.items.map(
+        ({ product, quantity, unitPrice } : {product: any ; quantity : number; unitPrice: number}) => ({
         productId: product._id,
         title: product.title,
         image: product.image,
         quantity,
-        unitPrice: product.unitPrice,
+        unitPrice,
       }));
 
       setCartItems(cartItemsMapped);
+      setTotalAmount(cart.totalAmount)
     };
 
     fetchCart();
