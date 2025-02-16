7a25a416b94f1113d589b9cc6d7545e86c7e387d
danyakorkmaz <106125107+danyakorkmaz@users.noreply.github.com>
2025-02-16 15:31:54 +0300

    checkout integration + order success page

--- frontend/src/pages/CheckoutPage.tsx
+++ frontend/src/pages/CheckoutPage.tsx
@@ -2,23 +2,53 @@ import { Box, Typography, Container, TextField } from "@mui/material";
 import { useCart } from "../context/Cart/CartContext";
 import Button from "@mui/material/Button";
 import { useRef } from "react";
+import { BASE_URL } from "../constants/baseUrl";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/Auth/AuthContext";
 
 const CheckoutPage = () => {
-  const {
-    cartItems,
-    totalAmount,
-  } = useCart();
+  const { cartItems, totalAmount } = useCart();
+
+  const { token } = useAuth();
 
   const addressRef = useRef<HTMLInputElement>(null);
 
+  const navigate = useNavigate();
+
+  const handleConfirmOrder = async () => {
+    const address = addressRef.current?.value;
+
+    if (!address) return;
+
+    //make the call to API to create the user
+    const response = await fetch(`${BASE_URL}/cart/checkout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        address,
+      }),
+    });
+
+    if (!response.ok) return;
+
+    navigate("/order-success");
+  };
+
   const renderCartItems = () => (
-    <Box display="flex" flexDirection="column" gap={2}
-    sx={{
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap={2}
+      sx={{
         border: 1,
         borderColor: "#f2f2f2",
         borderRadius: 5,
         padding: 1,
-      }}>
+      }}
+    >
       {cartItems.map((item) => (
         <Box
           display="flex"
@@ -26,26 +56,42 @@ const CheckoutPage = () => {
           justifyContent="space-between"
           alignItems="center"
           width="100%"
-          
         >
-          <Box display="flex" flexDirection="row" alignItems="center" gap={1} width="100%">
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            gap={1}
+            width="100%"
+          >
             <img src={item.image} width={50} />
-            <Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between" width="100%">
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="space-between"
+              width="100%"
+            >
               <Typography variant="h6">{item.title}</Typography>
               <Typography>
                 {item.quantity} x {item.unitPrice} EGP{" "}
               </Typography>
             </Box>
           </Box>
-       </Box>
+        </Box>
       ))}
-      <Box >
-        <Typography variant="body2" sx={{textAlign: 'right'}}>Total Amount: {totalAmount.toFixed(2)}EGP</Typography>
+      <Box>
+        <Typography variant="body2" sx={{ textAlign: "right" }}>
+          Total Amount: {totalAmount.toFixed(2)}EGP
+        </Typography>
       </Box>
     </Box>
   );
   return (
-    <Container fixed sx={{ mt: 2, display:"flex" , flexDirection:"column", gap: 2 }}>
+    <Container
+      fixed
+      sx={{ mt: 2, display: "flex", flexDirection: "column", gap: 2 }}
+    >
       <Box
         display="flex"
         flexDirection="row"
@@ -54,10 +100,17 @@ const CheckoutPage = () => {
       >
         <Typography variant="h4">Checkout</Typography>
       </Box>
-<TextField inputRef={addressRef} label=" Delivery Address" name="address" fullWidth/>
+      <TextField
+        inputRef={addressRef}
+        label=" Delivery Address"
+        name="address"
+        fullWidth
+      />
       {renderCartItems()}
-      
-      <Button variant="contained" fullWidth >Pay now</Button>
+
+      <Button variant="contained" fullWidth onClick={handleConfirmOrder}>
+        Pay now
+      </Button>
     </Container>
   );
 };
